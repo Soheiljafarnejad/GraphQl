b1913c629a8a23364740f1687a99e36cb4ad5f6c
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-06-04 19:15:52 +0430

    useQuery & variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { gql, useQuery } from "@apollo/client";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world!</div>;
+  const GET_USER = gql`
+    query($id:ID!) {
+      user(id : $id) {
+        id
+        email
+        username
+      }
+    }
+  `;
+
+  const { loading, error, data } = useQuery(GET_USER, {
+    variables: { id: 2 },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
+
+  console.log(data);
+
+  return (
+    <div className="App">
+      {data && (
+        <div>
+          <p>{data.user.id}</p>
+          <p>{data.user.username}</p>
+          <p>{data.user.email}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
