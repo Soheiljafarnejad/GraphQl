6dd87797018f04c4ed8260771511c27243893ea9
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-06-04 19:39:13 +0430

    pollInterval & refetch

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 
 function App() {
   const GET_USER = gql`
-    query($id:ID!) {
-      user(id : $id) {
+    query ($id: ID!) {
+      user(id: $id) {
         id
         email
         username
@@ -12,14 +12,16 @@ function App() {
     }
   `;
 
-  const { loading, error, data } = useQuery(GET_USER, {
-    variables: { id: 2 },
+  const { loading, error, data, refetch } = useQuery(GET_USER, {
+    variables: { id: 1 },
+    pollInterval: 500,
   });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
 
   console.log(data);
+  console.log("render");
 
   return (
     <div className="App">
@@ -30,6 +32,7 @@ function App() {
           <p>{data.user.email}</p>
         </div>
       )}
+      <button onClick={() => refetch({ id: 2 })}>refetch</button>
     </div>
   );
 }
